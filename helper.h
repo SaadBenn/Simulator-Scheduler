c7762437a262a89786dee3d37038af24eb1cd0f0
Saad <saadbennmushtaq@gmail.com>
2018-06-24 00:27:11 -0500

    Include functions required for scheduling
    - generating a random number
    - Incomplete FCFS function

--- helper.h
+++ helper.h
@@ -1,22 +1,21 @@
-#define LENGTH 15
+#define LENGTH 64
 #define PROCESS_SIZE 50 
 
 
 // Global variables
 int _priority;
 int _odds_of_IO;
-char _thread_name[20];
-char _thread_type[15];
+char *_thread_name;
+char *_thread_type;
 int _thread_length;
-Process_queue queue;
 
 
 // structs
 typedef struct process {
 	int priority;
-	char thread_name[20];
+	char *thread_name;
 	int thread_length;
-	char thread_type[15];
+	char *thread_type;
 	int odds_of_IO;	
 } Process;
 
@@ -33,14 +32,19 @@ typedef struct process_queue{
 	struct process_node *back;
 } Process_queue;
 
+Process_queue *queue;
 
 // Queue management functions
 Process_node *create_process_node(Process *);
-void initialize_process_queue(Process_queue *);
+Process_queue *initialize_process_queue(Process_queue *);
 void enqueue_process(Process_queue *, Process *);
-
+void dequeue_process(Process_queue *);
 
 // Process loading function
-void load_processes(char *);
+void load_processes(char *, Process_queue *);
 bool process_line(char *);
 bool create_process();
+
+// scheduler management functions
+int generate_random_word(int len);
+void process_fcfs(Process_queue *);
